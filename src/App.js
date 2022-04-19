62d9b8c11300347b3e5877a26884a5ed4463989e
Dominic Whelan <dominic.whelan27@gmail.com>
2022-04-19 10:34:59 -0230

    The start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">My Portfolio</header>
+        <div className="Body">
+          <h1>Dominic Whelan</h1>
+          <Link to="/">Home</Link> <br />
+          <Link to="/Components/Projects">My Projects</Link>
+        </div>
+        <Routes>
+          <Route path="/Components/Projects" element={<Projects />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>My Software Development Projects</h1>
+      <div>
+        <h2>Challenge Projects</h2>
+        <div>
+          <h3>Semester 1 Challenge 1 - "4 in a row" game</h3>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
